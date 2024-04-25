3f38722541a3322ce8f6fd560445601184994bd7
Lukim9 <86387262+Lukim99@users.noreply.github.com>
2024-04-25 19:51:52 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 allWords = [];
 rawFile = new XMLHttpRequest();
-rawFile.open("GET", "https://word.old-stdict-korean.kro.kr", false);
+rawFile.open("GET", "https://old-stdict-korean.kro.kr/allWords.txt", false);
 rawFile.onreadystatechange = function ()
 {
     if(rawFile.readyState === 4)
